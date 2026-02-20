b20ae0f6bc8f957b463cde5afcf353e941b166f1
Bruno Mendez <240846@utxicotepec.edu.mx>
2026-02-20 14:31:50 -0600

    falta arreglar la linea esa

--- my-react-app/src/Productos.jsx
+++ my-react-app/src/Productos.jsx
@@ -1,73 +1,40 @@
-import polishImage from "./assets/polishPleasing.avif"
-import vinylHHImage from "./assets/vinylHarrysHouse.jpg"
-import HS1Image from "./assets/HS1.jpg"
-import finelineImage from './assets/finelineVinyl.webp'
-import sudaderaConejosImage from './assets/sudaderaConejos.jpg'
-import sudaderaHarryImage from './assets/sudaderaHarry.webp'
-import cassetteHHImage from './assets/cassetteHH.webp'
 
-import "./Productos.css"
+import {useEffect, useState} from 'react';
+import api from './Services/api';
+import './Productos.css';
 
-function Productos (){
+function Productos(){
+    const [productos, setProductos] = useState([]);
+    const [cargando, setCargando] = useState(true);
+    useEffect(()=> {
+        const obtenerProductos = async () => {
+            try {
+                const response =await api.get('/products');
+                setProductos(response.data)
+            }catch(error){
+                console.error('Error al obtener el producto:', error);
+            }finally{
+                setCargando(false);
+            }
+        };
+        obtenerProductos();
+        },[]);
+
+        if(cargando) return<p>Cargando Productos...</p>
     return (
-        <>
-            <h2 className="section-title">Productos</h2>
+        <div>
+            <h2>Productos destacados</h2>
+            {productos.map((producto) => (
+                <div key={producto.id}>
+                    <p>{producto.title}</p>
+                    <p>{producto.price}</p>
+                    <img src={producto.image}/>
+                </div>
 
-            <div className="productos-grid">
-                <article className="producto-card">
-                    <img src={vinylHHImage} alt="Disco Vinyl Harry's House" />
-                    <h3>Disco de Vinyl del album Harry's House</h3>
-                    <p>Disco con photobook incluido</p>
-                    <button className="btn">Ver producto</button>
-                </article>
-
-                <article className="producto-card">
-                    <img src={HS1Image} alt="Disco Vinyl Harry Styles" />
-                    <h3>Disco de Vinyl del album Harry Styles</h3>
-                    <p>Vinilo de colección con edición remasterizada.</p>
-                    <button className="btn">Ver producto</button>
-                </article> 
-
-                <article className="producto-card">
-                    <img src={finelineImage} alt="Disco Vinyl Fine Line" />
-                    <h3>Disco de Vinyl del album Fine Line</h3>
-                    <p>Vinilo de colección con edición remasterizada.</p>
-                    <button className="btn">Ver producto</button>
-                </article> 
-
-                 <article className="producto-card">
-                    <img src={sudaderaConejosImage} alt="Sudadera" />
-                    <h3>Sudadera Love On Tour</h3>
-                    <p> Sudadera con estampado de conejos.</p>
-                    <button className="btn">Ver producto</button>
-                </article> 
-                
-
-                 <article className="producto-card">
-                    <img src={sudaderaHarryImage} alt="Sudadera" />
-                    <h3>Sudadera "Harry"</h3>
-                    <p>Sudadera del primer tour de Harry Styles</p>
-                    <button className="btn">Ver producto</button>
-                </article> 
-                
-
-                 <article className="producto-card">
-                    <img src={cassetteHHImage} alt="Cassette" />
-                    <h3>Cassette</h3>
-                    <p>Cassette del album Harry's House.</p>
-                    <button className="btn">Ver producto</button>
-                </article> 
-                
-                <article className="producto-card">
-                    <img src={polishImage} alt="Esmalte Pleasing" />
-                    <h3>Esmalte para uñas con colores variados</h3>
-                    <p>Todo para mostrar tus manos con seguridad y amor</p>
-                    <button className="btn">Ver producto</button>
-                </article>
-
-            </div>
-        </>
+            ))}
+        </div>
     );
+     
 }
 
 export default Productos;
